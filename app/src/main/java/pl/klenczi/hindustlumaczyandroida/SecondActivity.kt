c9844916e22a6d3c06f8cdf5bf98aa695662d962
bartekkleczar <kleczarbartosz.pv@gmail.com>
2023-03-25 22:26:59 +0100

    add secondActivity

---
+++ app/src/main/java/pl/klenczi/hindustlumaczyandroida/SecondActivity.kt
@@ -0,0 +1,11 @@
+package pl.klenczi.hindustlumaczyandroida
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+    }
+}
