cd8545ff88fac963ade738e3ab1f8b4df8175c84
bartekkleczar <kleczarbartosz.pv@gmail.com>
2023-03-26 14:16:58 +0200

    6 greetings from india

--- app/src/main/java/pl/klenczi/hindustlumaczyandroida/MainActivity.kt
+++ app/src/main/java/pl/klenczi/hindustlumaczyandroida/MainActivity.kt
@@ -2,10 +2,22 @@ package pl.klenczi.hindustlumaczyandroida
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var greetingsFromIndia = findViewById<TextView>(R.id.tvHello)
+        var inputField = findViewById<EditText>(R.id.tvName)
+        var submitButton = findViewById<Button>(R.id.btnSubmit)
+        submitButton.setOnClickListener{
+            var enteredName = inputField.text.toString()
+            var message = "Greetings from India $enteredName"
+            greetingsFromIndia.text = message
+        }
     }
 }
