2cb3a24bccc2ac1c7f3cdf75f9f90032cbbb7fa6
bartekkleczar <kleczarbartosz.pv@gmail.com>
2023-03-26 14:27:28 +0200

    7 cos tam

--- app/src/main/java/pl/klenczi/hindustlumaczyandroida/MainActivity.kt
+++ app/src/main/java/pl/klenczi/hindustlumaczyandroida/MainActivity.kt
@@ -1,5 +1,6 @@
 package pl.klenczi.hindustlumaczyandroida
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -7,12 +8,13 @@ import android.widget.EditText
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         var greetingsFromIndia = findViewById<TextView>(R.id.tvHello)
-        var inputField = findViewById<EditText>(R.id.tvName)
+        var inputField = findViewById<EditText>(R.id.etName)
         var submitButton = findViewById<Button>(R.id.btnSubmit)
         submitButton.setOnClickListener{
             var enteredName = inputField.text.toString()
