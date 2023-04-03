0e7ee018b0ab023738428e6f46a014e997a53d74
bartekkleczar <kleczarbartosz.pv@gmail.com>
2023-04-03 21:19:20 +0200

    added SpeedControler interface - MyCar class

--- app/src/main/java/pl/klenczi/oopbyhindus/MyCar.kt
+++ app/src/main/java/pl/klenczi/oopbyhindus/MyCar.kt
@@ -2,8 +2,16 @@ package pl.klenczi.oopbyhindus
 
 import android.util.Log
 
-class MyCar: Car() {
+class MyCar: Car(), SpeedController {
     override fun start(){
-        Log.i("MyTag", "this is MyCar Starting...")
+        Log.i("MyTag", "this is MyCar Starting... brand id is ${getBrandId()}")
+    }
+
+    override fun accelerate() {
+
+    }
+
+    override fun decelerate() {
+
     }
 }
