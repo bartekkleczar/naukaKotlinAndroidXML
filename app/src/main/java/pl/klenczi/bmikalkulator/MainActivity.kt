c193dfadfb858c0380eb19bd824ad3da564592df
bartekkleczar <kleczarbartosz.pv@gmail.com>
2023-04-05 22:45:16 +0200

    zabezpieczenie przed nullami w inpucie - MainActivity.kt

--- app/src/main/java/pl/klenczi/bmikalkulator/MainActivity.kt
+++ app/src/main/java/pl/klenczi/bmikalkulator/MainActivity.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,13 +20,28 @@ class MainActivity : AppCompatActivity() {
         calcButton.setOnClickListener{
             val weight = weightText.text.toString()
             val height = heightText.text.toString()
-
-            val bmi = weight.toFloat()/((height.toFloat()/100)*(height.toFloat()/100))
-            val bmi2D = String.format("%.2f", bmi).toFloat()
-
-            displayResult(bmi2D)
+            if(validateInput(weight, height)){
+                val bmi = weight.toFloat()/((height.toFloat()/100)*(height.toFloat()/100))
+                val bmi2D = String.format("%.2f", bmi).toFloat()
+                displayResult(bmi2D)
+            }
         }
     }
+
+    private fun validateInput(weight: String?, height: String?): Boolean{
+        return when{
+            weight.isNullOrEmpty() -> {
+                Toast.makeText(this, "Weight is empty", Toast.LENGTH_LONG).show()
+                return false}
+
+            height.isNullOrEmpty() -> {
+                Toast.makeText(this, "Height is empty", Toast.LENGTH_LONG).show()
+                return false}
+
+            else{return true}
+        }
+    }
+
     private fun displayResult(bmi: Float){
         val resultIndex = findViewById<TextView>(R.id.tvIndex)
         val resultDescription = findViewById<TextView>(R.id.tvResult)
@@ -32,6 +49,19 @@ class MainActivity : AppCompatActivity() {
 
         resultIndex.text = bmi.toString()
         info.text = "Normal range is 18.5 - 24.9"
+
+        var resultText = ""
+        var color = 0
+
+        when{
+            bmi < 18.50 -> {resultText = "Underweight"; color = R.color.underWeight}
+            bmi in 18.50 .. 24.99 -> {resultText = "Healthy"; color = R.color.normal}
+            bmi in 25.00 .. 29.99 -> {resultText = "Over Weight"; color = R.color.overWeight}
+            bmi > 29.99 -> {resultText = "Obese"; color = R.color.obese}
+        }
+
+        resultDescription.setTextColor(ContextCompat.getColor(this, color))
+        resultDescription.text = resultText
     }
 
 }
