692c1e20a1ba72c9c0d8dcb68d86ff2ad31ceb57
bartekkleczar <kleczarbartosz.pv@gmail.com>
2023-04-06 20:54:27 +0200

    jakies zmiany w pliku xml idc

--- app/src/main/java/pl/klenczi/clapapp/MainActivity.kt
+++ app/src/main/java/pl/klenczi/clapapp/MainActivity.kt
@@ -1,11 +1,14 @@
 package pl.klenczi.clapapp
 
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var mP: MediaPlayer
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        mP = mP.create(this,  R.raw.applauding)
     }
 }
