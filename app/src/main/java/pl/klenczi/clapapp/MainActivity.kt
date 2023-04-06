37063ac42c096cdab18f7df36617a28862be46e2
bartekkleczar <kleczarbartosz.pv@gmail.com>
2023-04-06 21:03:44 +0200

    kod do przycisku otworzenia dzwięku - activity_main.xml

--- app/src/main/java/pl/klenczi/clapapp/MainActivity.kt
+++ app/src/main/java/pl/klenczi/clapapp/MainActivity.kt
@@ -3,12 +3,19 @@ package pl.klenczi.clapapp
 import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     private lateinit var mP: MediaPlayer
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        mP = mP.create(this,  R.raw.applauding)
+        mP = MediaPlayer.create(this,  R.raw.applauding)
+
+        val button = findViewById<Button>(R.id.btnClap)
+        button.setOnClickListener{
+            mP.start()
+        }
+
     }
 }
