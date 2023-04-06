7815dfbcf89699dd240c2b2df897929cbb147623
bartekkleczar <kleczarbartosz.pv@gmail.com>
2023-04-06 22:32:16 +0200

    kod do seekBar - MainActivity.kt

--- app/src/main/java/pl/klenczi/clapapp/MainActivity.kt
+++ app/src/main/java/pl/klenczi/clapapp/MainActivity.kt
@@ -3,15 +3,23 @@ package pl.klenczi.clapapp
 import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.widget.Button
+import android.widget.SeekBar
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var seekBar: SeekBar
     private var mP: MediaPlayer? = null
+    private lateinit var runnable: Runnable
+    private lateinit var handler: Handler
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        seekBar = findViewById(R.id.sbClapping)
+        handler = Handler(Looper.getMainLooper())
         val play = findViewById<FloatingActionButton>(R.id.fabPlay)
         val pause = findViewById<FloatingActionButton>(R.id.fabPause)
         val stop = findViewById<FloatingActionButton>(R.id.fabStop)
@@ -19,6 +27,7 @@ class MainActivity : AppCompatActivity() {
         play.setOnClickListener {
             if(mP == null){
                 mP = MediaPlayer.create(this,  R.raw.applauding)
+                initSeekBar()
             }
 
             mP?.start()
@@ -33,6 +42,28 @@ class MainActivity : AppCompatActivity() {
             mP?.reset()
             mP?.release()
             mP = null
+            handler.removeCallbacks(runnable)
+            seekBar.progress = 0
         }
+    }
+    private fun initSeekBar(){
+        seekBar.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                if(fromUser) mP?.seekTo(progress)
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+            }
+        })
+        seekBar.max = mP!!.duration
+        runnable = Runnable{
+            seekBar.progress = mP!!.currentPosition
+            handler.postDelayed(runnable, 1000)
+        }
+        handler.postDelayed(runnable, 1000)
+
     }
 }
