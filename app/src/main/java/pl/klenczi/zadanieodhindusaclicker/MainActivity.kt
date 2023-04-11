5c44a087b1a98ef090b4235ce3c5c2e55bf21c48
bartekkleczar <kleczarbartosz.pv@gmail.com>
2023-04-11 15:07:05 +0200

    usuniecie bindingu

--- app/src/main/java/pl/klenczi/zadanieodhindusaclicker/MainActivity.kt
+++ app/src/main/java/pl/klenczi/zadanieodhindusaclicker/MainActivity.kt
@@ -2,20 +2,26 @@ package pl.klenczi.zadanieodhindusaclicker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import pl.klenczi.zadanieodhindusaclicker.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    var count = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.btnCount.setOnClickListener {
-            when(binding.tvCount.text.toString() ){
-                "" -> {binding.tvCount.text = "1"}
-                else -> {binding.tvCount.text = (binding.tvCount.text.toString().toInt()+1).toString()}
-            }
+        val tv = findViewById<TextView>(R.id.tvCount)
+        val btn = findViewById<Button>(R.id.btnCount)
+
+        tv.text = count.toString()
+
+        btn.setOnClickListener{
+            ++count
+            tv.text = count.toString()
         }
     }
 }
