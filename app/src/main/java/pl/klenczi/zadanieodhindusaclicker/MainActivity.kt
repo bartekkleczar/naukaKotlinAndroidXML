4b86760c981f547c4478e191533f238206bec34c
bartekkleczar <kleczarbartosz.pv@gmail.com>
2023-04-11 15:18:41 +0200

    zastosowanie viewModel w MainActivity.kt dla zmiennej count

--- app/src/main/java/pl/klenczi/zadanieodhindusaclicker/MainActivity.kt
+++ app/src/main/java/pl/klenczi/zadanieodhindusaclicker/MainActivity.kt
@@ -4,24 +4,24 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import androidx.lifecycle.ViewModelProvider
 import pl.klenczi.zadanieodhindusaclicker.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
     var count = 0
+    private lateinit var viewModel: MainActivityViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
+        setContentView(R.layout.activity_main)
+        viewModel = ViewModelProvider(this).get(MainActivityViewModel::class.java)
         val tv = findViewById<TextView>(R.id.tvCount)
         val btn = findViewById<Button>(R.id.btnCount)
 
-        tv.text = count.toString()
+        tv.text = viewModel.count.toString()
 
         btn.setOnClickListener{
-            ++count
-            tv.text = count.toString()
+            viewModel.updateCount()
+            tv.text = viewModel.count.toString()
         }
     }
 }
