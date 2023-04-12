849d520760c8982b7a60aebc012f6a9216bb271c
bartekkleczar <kleczarbartosz.pv@gmail.com>
2023-04-12 20:08:02 +0200

    dodanie koloru i layoutManagera do rv

--- app/src/main/java/pl/klenczi/hindustlumaczyrecyclerview/MainActivity.kt
+++ app/src/main/java/pl/klenczi/hindustlumaczyrecyclerview/MainActivity.kt
@@ -1,11 +1,18 @@
 package pl.klenczi.hindustlumaczyrecyclerview
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val rv = findViewById<RecyclerView>(R.id.myRV)
+        rv.setBackgroundColor(Color.YELLOW)
+        rv.layoutManager = LinearLayoutManager(this)
     }
 }
