d642dc7b433a2c2cd656ae6ee5485d670b5e33fc
bartekkleczar <kleczarbartosz.pv@gmail.com>
2023-04-12 20:13:26 +0200

    dodanie klasy adaptera i holdera do rv

--- app/src/main/java/pl/klenczi/hindustlumaczyrecyclerview/MyRvAdapter.kt
+++ app/src/main/java/pl/klenczi/hindustlumaczyrecyclerview/MyRvAdapter.kt
@@ -1,2 +1,23 @@
 package pl.klenczi.hindustlumaczyrecyclerview
 
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class MyRvAdapter: RecyclerView.Adapter<MyVHolder>(){
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyVHolder {
+
+    }
+
+    override fun onBindViewHolder(holder: MyVHolder, position: Int) {
+    }
+
+    override fun getItemCount(): Int {
+        return 5
+    }
+
+}
+
+class MyVHolder(val view: View):RecyclerView.ViewHolder(view){
+
+}
