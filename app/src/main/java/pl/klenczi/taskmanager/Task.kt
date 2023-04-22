97baec92f3573e5cb96ce07d94b3a0f6e612ed1c
bartekkleczar <kleczarbartosz.pv@gmail.com>
2023-04-22 23:35:06 +0200

    zmiana wygladu i zmniejszenie marginow

---
+++ app/src/main/java/pl/klenczi/taskmanager/Task.kt
@@ -0,0 +1,14 @@
+package pl.klenczi.taskmanager
+
+class Task(var name: String, var isDone: Boolean = false){
+    private fun check(status: Boolean): Boolean {
+        when(status){
+            true -> {
+                return true
+            }
+            false -> {
+                return false
+            }
+        }
+    }
+}
