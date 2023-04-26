ae57b9d19d2c936613e41a004e62207d2c88bae1
bartekkleczar <kleczarbartosz.pv@gmail.com>
2023-04-26 20:04:37 +0200

    dodnaie funkcji laczacej z api

--- app/src/main/java/pl/klenczi/apitutorial/MainActivity.kt
+++ app/src/main/java/pl/klenczi/apitutorial/MainActivity.kt
@@ -3,6 +3,8 @@ package pl.klenczi.apitutorial
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import pl.klenczi.apitutorial.databinding.ActivityMainBinding
+import java.net.URL
+import javax.net.ssl.HttpsURLConnection
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding // Dodanie bindingu
@@ -10,5 +12,22 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater) // Dodanie bindingu - rozwianie go
         setContentView(binding.root) // Dodanie bindingu - zmiana content view
+
+        FetchCurrencyData().start()
+    }
+
+    private fun FetchCurrencyData(): Thread {
+        return Thread{
+            val url = URL("https://open.er-api.com/v6/latest/aud")
+            val connection = url.openConnection() as HttpsURLConnection
+
+            if(connection.responseCode == 200){
+                val inputSystem = connection.inputStream
+                println(inputSystem.toString())
+            }
+            else{
+                binding.baseCurrency.text = "Failed Connection"
+            }
+        }
     }
 }
