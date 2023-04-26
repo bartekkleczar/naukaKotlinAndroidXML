6c9c1f7a3f1766261033c231669939cf6cd7e47a
bartekkleczar <kleczarbartosz.pv@gmail.com>
2023-04-26 20:57:32 +0200

    dodanie bindingu

--- app/src/main/java/pl/klenczi/apitutorial/DrugieApi.kt
+++ app/src/main/java/pl/klenczi/apitutorial/DrugieApi.kt
@@ -2,10 +2,13 @@ package pl.klenczi.apitutorial
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import pl.klenczi.apitutorial.databinding.ActivityMainBinding
 
 class DrugieApi : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding // Dodanie bindingu
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_drugie_api)
+        binding = ActivityMainBinding.inflate(layoutInflater) // Dodanie bindingu - rozwianie go
+        setContentView(binding.root) // Dodanie bindingu - zmiana content view
     }
 }
