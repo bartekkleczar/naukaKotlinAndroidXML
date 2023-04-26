8e3df0f76c4ee2ccba824e6e01be37b95e061da7
bartekkleczar <kleczarbartosz.pv@gmail.com>
2023-04-26 20:56:03 +0200

    dodanie utworzenie DA

---
+++ app/src/main/java/pl/klenczi/apitutorial/DrugieApi.kt
@@ -0,0 +1,11 @@
+package pl.klenczi.apitutorial
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DrugieApi : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_drugie_api)
+    }
+}
