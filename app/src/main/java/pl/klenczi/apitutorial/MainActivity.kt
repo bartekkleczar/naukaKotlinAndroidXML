2abeb206da1a0d08d6f92be18a129fe6de42c98c
bartekkleczar <kleczarbartosz.pv@gmail.com>
2023-04-26 19:54:38 +0200

    dodanie bindingu do MA

--- app/src/main/java/pl/klenczi/apitutorial/MainActivity.kt
+++ app/src/main/java/pl/klenczi/apitutorial/MainActivity.kt
@@ -2,10 +2,13 @@ package pl.klenczi.apitutorial
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import pl.klenczi.apitutorial.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding // Dodanie bindingu
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater) // Dodanie bindingu - rozwianie go
+        setContentView(binding.root) // Dodanie bindingu - zmiana content view
     }
 }
