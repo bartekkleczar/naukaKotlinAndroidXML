94cc485ee2499ddcf0b72daee3df0ffcf9ed78a5
bartekkleczar <kleczarbartosz.pv@gmail.com>
2023-05-01 13:18:44 +0200

    naprawa burdelu

--- app/src/main/java/pl/klenczi/studentregister/MainActivity.kt
+++ app/src/main/java/pl/klenczi/studentregister/MainActivity.kt
@@ -2,6 +2,7 @@ package pl.klenczi.studentregister
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import pl.klenczi.studentregister.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
