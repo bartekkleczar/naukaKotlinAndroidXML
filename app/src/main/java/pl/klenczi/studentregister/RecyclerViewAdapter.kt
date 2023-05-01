650500ed71c18fc3f5f91f8ec0bdba0639918ec4
bartekkleczar <kleczarbartosz.pv@gmail.com>
2023-05-01 15:36:14 +0200

    simple Rv

---
+++ app/src/main/java/pl/klenczi/studentregister/RecyclerViewAdapter.kt
@@ -0,0 +1,41 @@
+package pl.klenczi.studentregister
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import pl.klenczi.studentregister.db.Student
+
+class RecyclerViewAdapter(): RecyclerView.Adapter<ViewHolder>() {
+    private val studentList = ArrayList<Student>()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val listItem = layoutInflater.inflate(R.layout.list_item, parent, false)
+        return ViewHolder(listItem)
+    }
+
+    override fun getItemCount(): Int {
+        return  studentList.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(studentList[position])
+    }
+
+    fun setList(students: List<Student>){
+        studentList.clear()
+        studentList.addAll(students)
+    }
+}
+
+class ViewHolder(private val view: View): RecyclerView.ViewHolder(view){
+    fun bind(student: Student){
+        val nameTV = view.findViewById<TextView>(R.id.tvName)
+        val emailTV = view.findViewById<TextView>(R.id.tvEmail)
+
+        nameTV.text = student.name
+        emailTV.text = student.email
+    }
+}
