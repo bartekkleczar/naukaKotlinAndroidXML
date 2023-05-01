810c35a737876b10cf4470d2747dc57b236c57d2
bartekkleczar <kleczarbartosz.pv@gmail.com>
2023-05-01 14:55:19 +0200

    override fun and added extras to fun

--- app/src/main/java/pl/klenczi/studentregister/StudentViewModelFactory.kt
+++ app/src/main/java/pl/klenczi/studentregister/StudentViewModelFactory.kt
@@ -2,10 +2,11 @@ package pl.klenczi.studentregister
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
 import pl.klenczi.studentregister.db.StudentDao
 
 class StudentViewModelFactory(private val dao: StudentDao):ViewModelProvider.Factory {
-    fun <T : ViewModel?> create(modelClass: Class<T>): T{
+    override fun <T : ViewModel> create(modelClass: Class<T>, extras: CreationExtras): T {
         if(modelClass.isAssignableFrom(StudentViewModel::class.java)){
             return StudentViewModel(dao) as T
         }
