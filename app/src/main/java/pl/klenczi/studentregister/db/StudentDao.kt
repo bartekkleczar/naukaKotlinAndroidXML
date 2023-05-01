72240f8b7400266d5a8ed5e84e181f87670253da
bartekkleczar <kleczarbartosz.pv@gmail.com>
2023-05-01 13:24:58 +0200

    dodanie komentarzy

--- app/src/main/java/pl/klenczi/studentregister/db/StudentDao.kt
+++ app/src/main/java/pl/klenczi/studentregister/db/StudentDao.kt
@@ -7,17 +7,18 @@ import androidx.room.Insert
 import androidx.room.Query
 import androidx.room.Update
 
-@Dao
-interface StudentDao {
-    @Insert
-    suspend fun insertStudent(student: Student)
+//Ten kod zawiera interfejs DAO (Data Access Object) dla bazy danych, która przechowuje informacje o studentach.
+@Dao // adnotacja oznaczająca, że to jest interfejs DAO.
+interface StudentDao { // deklaracja interfejsu DAO.
+    @Insert // metoda w interfejsie DAO do wstawiania danych w bazie danych
+    suspend fun insertStudent(student: Student) // stworzenie metody 'suspend' -> korutynowej
 
-    @Update
-    suspend fun updateStudent(student: Student)
+    @Update // metoda w interfejsie DAO do aktualizowania a danych w bazie danych
+    suspend fun updateStudent(student: Student) // stworzenie metody 'suspend' -> korutynowej
 
-    @Delete
-    suspend fun deleteStudent(student: Student)
+    @Delete // metoda w interfejsie DAO do usuwaniaa danych w bazie danych
+    suspend fun deleteStudent(student: Student) // stworzenie metody 'suspend' -> korutynowej
 
-    @Query("SELECT * FROM student_data_table")
-    fun getAllStudents(): LiveData<List<Student>>
+    @Query("SELECT * FROM student_data_table") // apytanie SQL do pobierania danych z bazy danych. Wybiera wszystkie wiersze z tabeli 'student_data_table'
+    fun getAllStudents(): LiveData<List<Student>> //  ta metoda zwraca wszystkich studentów z bazy danych jako <List<Student>>
 }
