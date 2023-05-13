6335cf42b07a232255b9c03018bf264a9641fc41
bartekkleczar <kleczarbartosz.pv@gmail.com>
2023-05-13 12:11:21 +0200

    zxamiana MA na binding

--- app/src/main/java/pl/klenczi/hindustlumaczyviewbinding/MainActivity.kt
+++ app/src/main/java/pl/klenczi/hindustlumaczyviewbinding/MainActivity.kt
@@ -5,20 +5,20 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import pl.klenczi.hindustlumaczyviewbinding.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
-        val messageView = findViewById<TextView>(R.id.tvMessage)
-        val inputText = findViewById<EditText>(R.id.etName)
-        val submitButton = findViewById<Button>(R.id.btnSubmit)
-
-        submitButton.setOnClickListener {
-            val name = inputText.text.toString()
-            messageView.text = "Hello $name"
-            inputText.setText("")
+        binding.btnSubmit.setOnClickListener {
+            val name = binding.etName.text.toString()
+            binding.tvMessage.text = "Hello $name"
+            binding.etName.setText("")
         }
     }
 }
