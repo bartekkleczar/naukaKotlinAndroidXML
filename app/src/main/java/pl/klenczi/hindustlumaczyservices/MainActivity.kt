60346ac1e047d44bbadf5c86524832c343de5db3
bartekkleczar <kleczarbartosz.pv@gmail.com>
2023-05-13 12:42:35 +0200

    obsluga service

--- app/src/main/java/pl/klenczi/hindustlumaczyservices/MainActivity.kt
+++ app/src/main/java/pl/klenczi/hindustlumaczyservices/MainActivity.kt
@@ -1,7 +1,9 @@
 package pl.klenczi.hindustlumaczyservices
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import pl.klenczi.hindustlumaczyservices.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -11,13 +13,16 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val serviceIntent = Intent(this, MyBackgroundService::class.java)
         binding.apply{
             btnStart.setOnClickListener{
-
+                Log.i("MainActivity", "Service starting")
+                startService(serviceIntent)
             }
 
             btnStop.setOnClickListener{
-
+                Log.i("MainActivity", "Service stopping")
+                stopService(serviceIntent)
             }
         }
     }
