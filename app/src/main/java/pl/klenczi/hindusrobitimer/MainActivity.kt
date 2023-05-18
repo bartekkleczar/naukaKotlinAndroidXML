64b4143634113ac17ebb957623af2fc6e355835a
bartekkleczar <kleczarbartosz.pv@gmail.com>
2023-05-18 16:23:10 +0200

    implemented binding in MA

--- app/src/main/java/pl/klenczi/hindusrobitimer/MainActivity.kt
+++ app/src/main/java/pl/klenczi/hindusrobitimer/MainActivity.kt
@@ -2,10 +2,13 @@ package pl.klenczi.hindusrobitimer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import pl.klenczi.hindusrobitimer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
